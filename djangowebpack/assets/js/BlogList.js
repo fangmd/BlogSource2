078275cab5dd6698aa71befd481a37c6bdd0cf72
Double <fangmingdong0224@gmail.com>
2018-03-14 21:39:46 +0800

    add

--- djangowebpack/assets/js/BlogList.js
+++ djangowebpack/assets/js/BlogList.js
@@ -5,6 +5,30 @@ import BlogItem from './BlogItem';
 
 var axios = require('axios');
 
+function my_nav(props){
+  const nextPage = new Number(this.props.pageIndex) + 1
+  const prePage = new Number(this.props.pageIndex) - 1
+  return (<h1> Title </h1>);
+  if(props.pageIndex == 0){
+    return (
+        <nav class="pagination" role="navigation">
+          <a class="btn-floating disabled"><i class="fa fa-angle-left"></i></a>
+          <span class="page-number">{this.props.pageIndex} / {this.state.totalPageCnt}</span>
+          <a class="btn-floating waves-effect red" href={"/blog/" + nextPage + "/"}><i class="fa fa-angle-right"></i></a>
+        </nav>
+      );
+  }else{
+    return(
+        <nav class="pagination" role="navigation">
+          <a class="btn-floating waves-effect red" href={"/blog/" + prePage + "/"}><i class="fa fa-angle-left"></i></a>
+          <span class="page-number">{this.props.pageIndex} / {this.state.totalPageCnt}</span>
+          <a class="btn-floating waves-effect red" href={"/blog/" + nextPage + "/"}><i class="fa fa-angle-right"></i></a>
+        </nav>
+      );
+  }
+  
+}
+
 class BlogList extends React.Component{
 
 	constructor(props){
@@ -38,6 +62,25 @@ class BlogList extends React.Component{
 
 	render(){
     const nextPage = new Number(this.props.pageIndex) + 1
+    const havePre = !(this.props.pageIndex <= 1)
+    const prePage = this.props.pageIndex - 1
+    this.props.nextPage = nextPage;
+
+    var navMy;
+    if(havePre){
+      navMy = <nav class='pagination' role='navigation'>
+          <a class='btn-floating waves-effect red' href={'/blog/' + prePage + '/'}><i class='fa fa-angle-left'></i></a>
+          <span class='page-number'>{this.props.pageIndex} / {this.state.totalPageCnt}</span>
+          <a class='btn-floating waves-effect red' href={'/blog/' + nextPage + '/'}><i class='fa fa-angle-right'></i></a>
+        </nav>
+    }else{
+      navMy = <nav class='pagination' role='navigation'>
+          <a class='btn-floating disabled'><i class='fa fa-angle-left'></i></a>
+          <span class='page-number'>{this.props.pageIndex} / {this.state.totalPageCnt}</span>
+          <a class='btn-floating waves-effect red' href={'/blog/' + nextPage + '/'}><i class='fa fa-angle-right'></i></a>
+        </nav>
+    }
+
 		return (
 			<div class="content pure-u-1 pure-u-md-3-4">
 
@@ -45,12 +88,8 @@ class BlogList extends React.Component{
           <BlogItem article={article}/>
         )}
 
-        <nav class="pagination" role="navigation">
-          <a class="btn-floating disabled"><i class="fa fa-angle-left"></i></a>
-          <span class="page-number">{this.props.pageIndex} / {this.state.totalPageCnt}</span>
-          <a class="btn-floating waves-effect red" href={"/blog/" + nextPage + "/"}><i class="fa fa-angle-right"></i></a>
-        </nav>
-
+        {navMy}
+        
 			</div>
 		);
 	}
